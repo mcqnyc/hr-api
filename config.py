19d27f4bb6f934af77e8220fd0786499c8b99a94
mcqnyc <mcqnyc@gmail.com>
2023-08-31 21:04:43 -0400

    refactor(env): configure envionment variable

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 import pathlib
 import connexion
 from flask_sqlalchemy import SQLAlchemy
@@ -7,7 +9,7 @@ basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql:///employee_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
