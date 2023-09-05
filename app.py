8b9ecb6c41a27ca902034af19346ace4f5e3ab30
mcqnyc <mcqnyc@gmail.com>
2023-09-05 16:56:32 -0400

    feat(observability): add status page endpoint

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def home():
     return render_template("home.html", employees=employees)
 
 
+@app.route("/status")
+def status():
+    return {"status": "OK"}
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
