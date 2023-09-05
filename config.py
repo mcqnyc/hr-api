b2665f857e4998adf7d7aa4a1867b0a2ade71f8d
mcqnyc <mcqnyc@gmail.com>
2023-09-05 16:45:42 -0400

    refactor(dotenv): add load_dotenv function

--- config.py
+++ config.py
@@ -4,11 +4,14 @@ import pathlib
 import connexion
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from dotenv import load_dotenv
 
 basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
+load_dotenv()
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
